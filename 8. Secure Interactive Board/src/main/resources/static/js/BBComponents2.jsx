8a20e6779dcefb7ab8807128238e49002d4163bc
Miguel Castellanos <miguel.castellanos@escuelaing.edu.co>
2021-07-08 16:46:09 -0500

    Avance

--- 8. Secure Interactive Board/src/main/resources/static/js/BBComponents2.jsx
+++ 8. Secure Interactive Board/src/main/resources/static/js/BBComponents2.jsx
@@ -113,6 +113,6 @@ class Editor extends React.Component {
 }
 
 ReactDOM.render(
-        <Editor name="Daniel"/>,
+        <Editor name="Miguel Castellanos"/>,
         document.getElementById('root')
         );
